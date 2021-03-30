6160210981fad7ba4ee5055089071f1a060e2030
RGBA-CRT <rgba3crt1p@gmail.com>
2021-03-31 03:26:28 +0900

    - add support BSM/SFT to Drag&Drop header info. - add version and contry code to save file name. - add ini option for SoundEnable, TrimRomEnable.

--- finalize_exe.bat
+++ finalize_exe.bat
@@ -1,4 +1,5 @@
-ffmpeg -i se\SE_OK_8bit.wav  -f wav -acodec adpcm_ms se\SE_OK.adpcm.wav
+REM ffmpeg -i se\SE_OK_16bit.wav  -f wav -acodec adpcm_ms se\SE_OK.adpcm.wav
+copy se\SE_OK_16bit.wav se\SE_OK.adpcm.wav
 ResourceHacker.exe -open resource_bin.res -save tmp.res -action addoverwrite -res se\SE_OK.adpcm.wav -mask WAVE,SE_OK,
 ResourceHacker.exe -open HongKongArduinoClone.exe -save HongKongArduinoClone.exe -action addoverwrite -res tmp.res
 del tmp.res
